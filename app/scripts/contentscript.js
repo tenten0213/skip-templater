63b043a97a33210d8e026872694628e9c4b9db4b
tenten0213 <takehito.0213@gmail.com>
2014-06-13 23:46:44 +0900

    show title and read template

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,47 +1,44 @@
 'use strict';
 
 $(function() {
-    chrome.storage.local.get(function(items) {
+    var storage = chrome.storage.local;
+    storage.get(function(items) {
 
-      var templates = items.templates;
-      console.log(templates);
       var $private_entries = $('.private_entries');
-
       var $template_entries = $('<div/>').attr('class', 'private_entries');
       var $title = $('<h2/>').attr('class', 'title').text('テンプレート');
       var $table = $('<table/>').attr('class', 'drafts').append('<tbody/>');
 
-      $(templates).each(function(index, template) {
+      $(Object.keys(items)).each(function(index, title) {
+        storage.get(title, function(template) {
+          var $tr = $('<tr/>').attr('class', 'topix_entry');
 
-        var $tr = $('<tr/>').attr('class', 'topix_entry');
+          var $td_buttons = $('<td/>').attr('class', 'buttons');
+          var $span_load = $('<span/>').attr('class', 'load');
 
-        var $td_buttons = $('<td/>').attr('class', 'buttons');
-        var $span_load = $('<span/>').attr('class', 'load');
+          // Chrome Extentionのcontent_scriptでは自分で定義した関数を呼び出せないので、無理矢理書き込んでいる
+          // template.bodyは改行コードが含まれるため、escapeしてデータを保存している。その為、.val()呼出し時にunescapeして\nに戻して出力している。
+          var $a_load = $('<a/>').attr('class', 'unbind_beforeunload').attr('href', '#').attr('onClick', '$("#board_entry_title").val("' + template[title].title + '"); $("#board_entry_contents").val(unescape("' + template[title].body + '")); return false;').text('読み込む');
+          var $span_delete= $('<span/>').attr('class', 'delete_operation_wrapper inline delete');
 
-        // Chrome Extentionのcontent_scriptでは自分で定義した関数を呼び出せないので、無理矢理書き込んでいる
-        // template.bodyは改行コードが含まれるため、escapeしてデータを保存している。その為、.val()呼出し時にunescapeして\nに戻して出力している。
-        var $a_load = $('<a/>').attr('class', 'unbind_beforeunload').attr('href', '#').attr('onClick', '$("#board_entry_title").val("' + template.title + '"); $("#board_entry_contents").val(unescape("' + template.body + '")); return false;').text('読み込む');
-        var $span_delete= $('<span/>').attr('class', 'delete_operation_wrapper inline delete');
-        var $a_delete= $('<a/>').attr('class', 'destroy_draft_trigger').attr('href', '#').attr('onClick', 'chrome.storage.local.remove(' + index + '); return false;').text('削除');
+          var $td_entry = $('<td/>').attr('class', 'entry');
+          // タイトルはlocalstorageから取得
+          var $span_entry = $('<span/>').attr('class', 'entry_title').text(template[title].title);
 
-        var $td_entry = $('<td/>').attr('class', 'entry');
-        // タイトルはlocalstorageから取得
-        var $span_entry = $('<span/>').attr('class', 'entry_title').text(template.title);
 
-        $template_entries.append($title);
+          $span_load.append($a_load);
+          $td_buttons.append($span_delete);
+          $td_buttons.append($span_load);
 
-        $span_delete.append($a_delete);
-        $span_load.append($a_load);
-        $td_buttons.append($span_delete);
-        $td_buttons.append($span_load);
+          $td_entry.append($span_entry);
 
-        $td_entry.append($span_entry);
+          $tr.append($td_buttons);
+          $tr.append($td_entry);
+          $table.append($tr);
+        });
+      });
 
-        $tr.append($td_buttons);
-        $tr.append($td_entry);
-        $table.append($tr);
-      })
-
+      $template_entries.append($title);
       $template_entries.append($table);
       $private_entries.append($template_entries);
 
--- app/scripts/options.js
+++ app/scripts/options.js
@@ -3,37 +3,39 @@ angular.module('skip-templater.options', ['ui.bootstrap'])
 
 .controller('OptionCtrl', ['$scope', function($scope) {
   $scope.alerts = [];
+  var storage = chrome.storage.local;
   $scope.addTemplate = function() {
     if($scope.title === undefined || $scope.title === "") {
       $scope.alerts.push({ type: 'warning', msg: 'タイトルは必須です' })
     } else {
-      chrome.storage.local.get(function(items) {
-        var templates = items.templates || [];
-        var body = escape($scope.body);
-        templates.push({ title: $scope.title, body: body });
-        chrome.storage.local.set({ templates:  templates}, function() {
-        });
-        console.log(templates);
+      saveLocalStorage();
+      storage.get(function(items) {
+         console.log(items);
       });
-      $scope.alerts.push({ type: 'success', msg: '登録しました' })
     }
   };
+
+  function saveLocalStorage() {
+      var template = { title: $scope.title, body: escape($scope.body) };
+      var data = {};
+      data[$scope.title] = template;
+      storage.set(data);
+      $scope.alerts.push({ type: 'success', msg: '登録しました' });
+  }
 
   $scope.clear = function() {
     $scope.title = "";
     $scope.body= "";
-    chrome.storage.local.get(function(items) {
-       var templates = items.templates || [];
-       templates = templates.filter(function(index) {
-          return index != 2;
-       });
-       console.log(templates);
-    });
   };
 
   $scope.allDelete = function() {
     if(window.confirm("本当に削除してよろしいですか？") == true) {
-      chrome.storage.local.clear();
+      storage.clear();
+      $scope.clear();
+      $scope.alerts.push({ type: 'danger', msg: '全件削除しました' })
+      storage.get(function(items) {
+         console.log(items);
+      });
     }
   };
 
